e02868ddd752f8cb54c697fa782714114012314c
sam j <jlatha541@gmail.com>
2025-02-14 21:47:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const videos = [
         "https://github.com/jsam-008/turf_web/blob/main/Football%20in%20slow%20motion%20-%20social%20media%20video%20ad%20-%20stock%20video.mp4,
-        "https://drive.google.com/file/d/1mKO__hNjyqG2loj88o3MeVt9NwQylBxz/view?usp=drive_link"
+        "https://github.com/jsam-008/turf_web/blob/main/Cricket%20Video%20Promo.mp4"
     ];
 
     let currentVideoIndex = 0;
