bbc38619c2dbc8abbc28f93ba7197000fe263130
sam j <jlatha541@gmail.com>
2025-02-14 21:42:01 +0530

    Resolved merge conflicts and updated files

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Script Loaded");
 
-    // Button Click Alert
-    document.querySelector(".cta-btn").addEventListener("click", function () {
-        alert("Booking feature coming soon!");
+
+    document.querySelectorAll(".cta-btn").forEach(button => {
+        button.addEventListener("click", function () {
+            alert("Booking will be available later.");
+        });
     });
 
-    // Hover Effect on Turf Cards
     const turfCards = document.querySelectorAll(".turf-card");
     turfCards.forEach(card => {
         card.addEventListener("mouseover", function () {
@@ -17,13 +18,21 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    // Video Play/Pause Toggle
-    const video = document.querySelector("video");
-    video.addEventListener("click", function () {
-        if (video.paused) {
-            video.play();
-        } else {
-            video.pause();
-        }
+    const videos = [
+        "https://drive.google.com/file/d/1ZrwtxVkGwFJStXO552843_OscOhvEu4Q/view?usp=drive_link",
+        "https://drive.google.com/file/d/1mKO__hNjyqG2loj88o3MeVt9NwQylBxz/view?usp=drive_link"
+    ];
+
+    let currentVideoIndex = 0;
+    const videoPlayer = document.getElementById("videoPlayer");
+    const videoSource = document.getElementById("videoSource");
+
+    videoPlayer.addEventListener("ended", function () {
+
+        currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+        videoSource.src = videos[currentVideoIndex];
+        videoPlayer.load();
+        videoPlayer.play();
     });
+
 });
