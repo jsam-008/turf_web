5e90eb136c1183f6bf78f691b4251dc8331d171c
sam j <jlatha541@gmail.com>
2025-02-19 22:07:14 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Script Loaded");
 
-
     document.querySelectorAll(".cta-btn").forEach(button => {
         button.addEventListener("click", function () {
             alert("Booking will be available later.");
@@ -11,16 +10,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const turfCards = document.querySelectorAll(".turf-card");
     turfCards.forEach(card => {
         card.addEventListener("mouseover", function () {
-            this.querySelector(".hover-img").style.opacity = "1";
+            const hoverImg = this.querySelector(".hover-img");
+            if (hoverImg) hoverImg.style.opacity = "1";
         });
         card.addEventListener("mouseout", function () {
-            this.querySelector(".hover-img").style.opacity = "0";
+            const hoverImg = this.querySelector(".hover-img");
+            if (hoverImg) hoverImg.style.opacity = "0";
         });
     });
 
     const videos = [
-        "C:\Users\91637\Downloads\Football in slow motion - social media video ad - stock video.mp4",
-        "C:\Users\91637\Downloads\Cricket Video Promo.mp4"
+        "file:///C:/Users/91637/Downloads/Football%20in%20slow%20motion%20-%20social%20media%20video%20ad%20-%20stock%20video.mp4",
+        "file:///C:/Users/91637/Downloads/Cricket%20Video%20Promo.mp4"
     ];
 
     let currentVideoIndex = 0;
@@ -28,11 +29,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const videoSource = document.getElementById("videoSource");
 
     videoPlayer.addEventListener("ended", function () {
-
         currentVideoIndex = (currentVideoIndex + 1) % videos.length;
         videoSource.src = videos[currentVideoIndex];
         videoPlayer.load();
         videoPlayer.play();
     });
-
 });
